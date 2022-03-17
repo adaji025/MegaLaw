5cde28ed27e31c0763ee244a78d7ba4e89dec779
Adaji Mukhtar <alfadaji@gmail.com>
2022-03-17 17:01:36 +0100

    added animated cursor

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import dynamic from 'next/dynamic'
 import "../styles/globals.css";
 import '../components/Nav/Navbar.css'
 import '../components/Contact/Contact.css'
@@ -7,9 +8,23 @@ import { useEffect } from 'react';
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     import('bootstrap/dist/js/bootstrap');
+    
   }, []);
+
+  const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
+    ssr: false
+  });
+  console.log(AnimatedCursor);
   return (
     <>
+   <AnimatedCursor
+      innerSize={18}
+      outerSize={8}
+      color='193, 11, 111'
+      outerAlpha={0.2}
+      innerScale={0.7}
+      outerScale={5}
+    />
       <Component {...pageProps} />
     </>
   );
